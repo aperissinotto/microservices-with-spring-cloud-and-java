3f15ea65285752eeb8b71f2b6c4a560e8c9052be
aperissinotto <alexandrepamaral@gmail.com>
2025-11-02 14:51:30 -0300

    new chapter

---
+++ 04_RestInPraticeWithSpringBoot/04A_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/perissinotto/math/SimpleMath.java
@@ -0,0 +1,27 @@
+package br.com.perissinotto.math;
+
+public class SimpleMath {
+    public Double sum(Double numberOne, Double numberTwo) {
+        return numberOne + numberTwo;
+    }
+
+    public Double subtract(Double numberOne, Double numberTwo) {
+        return numberOne - numberTwo;
+    }
+
+    public Double multiplication(Double numberOne, Double numberTwo) {
+        return numberOne * numberTwo;
+    }
+
+    public Double division(Double numberOne, Double numberTwo) {
+        return numberOne / numberTwo;
+    }
+
+    public Double mean(Double numberOne, Double numberTwo) {
+        return (numberOne + numberTwo) / 2;
+    }
+
+    public Double squareroot(Double number) {
+        return Math.sqrt(number);
+    }
+}
