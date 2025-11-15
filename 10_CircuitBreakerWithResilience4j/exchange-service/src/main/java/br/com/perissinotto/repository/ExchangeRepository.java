bb31902875baf4cf3c5e9e49a18af60d3849f783
aperissinotto <alexandrepamaral@gmail.com>
2025-11-15 11:50:46 -0300

    teste CD

---
+++ 10_CircuitBreakerWithResilience4j/exchange-service/src/main/java/br/com/perissinotto/repository/ExchangeRepository.java
@@ -0,0 +1,9 @@
+package br.com.perissinotto.repository;
+
+import br.com.perissinotto.model.Exchange;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExchangeRepository extends JpaRepository<Exchange, Long> {
+    Exchange findByFromAndTo(String from, String to);
+}
+
